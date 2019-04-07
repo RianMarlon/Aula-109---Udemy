d0f3ed85b1493665f56b8933eea7871f9e6ce275
Rian Marlon <rianmarlon20@yahoo.com>
2019-04-07 13:00:01 -0300

    Atualizando seção 16

--- secao16.AcessoBancoDadosJDBC/aplicacao16/Principal5.java
+++ secao16.AcessoBancoDadosJDBC/aplicacao16/Principal5.java
@@ -1,9 +1,41 @@
 package aplicacao16;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db16.DB;
+import db16.DbIntegrityException;
+
 public class Principal5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"DELETE FROM  department "
+					+ "WHERE "
+					+ "Id = ?"
+					);
+			st.setInt(1, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException (e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 
 	}
 
